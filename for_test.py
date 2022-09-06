840675612e926079e8b93613fd7dc0c5887a262a
KyanChen <keyan1055002755@163.com>
2022-09-06 21:45:07 +0800

    add

--- for_test.py
+++ for_test.py
@@ -3,8 +3,9 @@ from torch import autocast
 from diffusers import StableDiffusionPipeline
 
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=True)
-pipe = pipe.to("cuda")
+device = 'cpu'
+pipe = pipe.to(device)
 
 prompt = "a photo of an astronaut riding a horse on mars"
-with autocast("cuda"):
+with autocast(device):
     image = pipe(prompt).images[0]
