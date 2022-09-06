6a79a629a614f7a624a6d7c1bc8aa16ffafd0348
KyanChen <keyan1055002755@163.com>
2022-09-06 21:18:28 +0800

    add

---
+++ for_test.py
@@ -0,0 +1,10 @@
+# make sure you're logged in with `huggingface-cli login`
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=True)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+with autocast("cuda"):
+    image = pipe(prompt).images[0]
